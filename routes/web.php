323d6316e5eafe97d266698da1576b108a455b4a
Hitoshi Watanabe <myah1938.dev@gmail.com>
2022-06-24 00:24:43 +0900

    react inertia導入

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,15 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::name('post.')
-    ->group(function() {
-        Route::get('/', 'App\Http\Controllers\PostController@index')->name('index');
-        Route::get('/post/{id}', 'App\Http\Controllers\PostController@show')->name('show');
-        
-    });
-    
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
