c98395c9bed797b49f6aba8716a2fa1445a0e7fc
Hitoshi Watanabe <myah1938.dev@gmail.com>
2022-07-10 17:50:54 +0900

    投稿機能作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,89 +2,107 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Log;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
+use Inertia\Inertia;
+// models
 use App\Models\Post;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('post.index', [
-            'posts' => Post::get()
-        ]);
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $posts = Post::all();
+    $images = array();
+    return Inertia::render('Home', [
+      'posts' => $posts
+    ]);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StorePostRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StorePostRequest $request)
-    {
-        //
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \App\Http\Requests\StorePostRequest  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(StorePostRequest $request)
+  {
+    Log::debug($request);
+    $images = $request->images;
+    $imagePath = '';
+    foreach ($images as $image) {
+      $imagePath = $image->store('image', 'public');
     }
+    Log::debug($imagePath);
+    Post::create([
+      'title' => $request->title,
+      'user_id' => $request->user_id,
+      'image_path' => $imagePath,
+      'ip_address' => 'default value'
+    ]);
+    return redirect()->route('post.index');
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        return view('post.show', [
-            'post' => Post::find($id),
-        ]);
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\Post  $post
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    return view('post.show', [
+      'post' => Post::find($id),
+    ]);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  \App\Models\Post  $post
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Post $post)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdatePostRequest  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdatePostRequest $request, Post $post)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \App\Http\Requests\UpdatePostRequest  $request
+   * @param  \App\Models\Post  $post
+   * @return \Illuminate\Http\Response
+   */
+  public function update(UpdatePostRequest $request, Post $post)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\Post  $post
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Post $post)
+  {
+    //
+  }
 }
--- resources/js/Pages/Post/Index.jsx
+++ resources/js/Pages/Post/Index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-
+import { useState } from "react";
+import axios from "axios";
 
 
 
 export default function Index(props) {
+
+  console.log(props)
   return (
     <>
-      INDEX
     </>
   )
 }
