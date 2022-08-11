885cf1893d414187ffc9a045130ab0208a8ce385
Hitoshi Watanabe <myah1938.dev@gmail.com>
2022-08-11 17:27:41 +0900

    インデント修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -17,11 +17,10 @@
     @routes
     <script src="{{ mix('js/app.js') }}" defer></script>
     @inertiaHead
-</head>
+  </head>
 
-<body class="font-sans antialiased">
+  <body class="font-sans antialiased">
     @inertia
-
-</body>
+  </body>
 
 </html>
